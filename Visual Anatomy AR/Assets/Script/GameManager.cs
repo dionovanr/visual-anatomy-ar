d96239d86df73b74af20bfec82b619870722746b
diraapriliats <apriliadira1@gmail.com>
2022-01-11 16:47:28 +0700

    Up Pankreas dan Paru paru

--- Visual Anatomy AR/Assets/Script/GameManager.cs
+++ Visual Anatomy AR/Assets/Script/GameManager.cs
@@ -18,6 +18,18 @@ public class GameManager : MonoBehaviour
         AudioManager.singleton.PlaySound(1);
     }
 
+    public void penjelasanPankreas()
+    {
+        AudioManager.singleton.stopSound();
+        AudioManager.singleton.PlaySound(5);
+    }
+
+    public void penjelasanParuparu()
+    {
+        AudioManager.singleton.stopSound();
+        AudioManager.singleton.PlaySound(6);
+    }
+
     public void penjelasanJantung()
     {
         AudioManager.singleton.stopSound();
