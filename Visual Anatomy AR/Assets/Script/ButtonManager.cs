3139f3ee916784b0d1399b61a4584841f4dfc918
Dionovan Ramadhani <80899280+dionovanr@users.noreply.github.com>
2022-01-16 10:55:27 +0700

    sound button changed

---
+++ Visual Anatomy AR/Assets/Script/ButtonManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject buttonAudio, buttonMute;
+    public float time;
+
+    public void changeTheButton()
+    {
+        Invoke("changeButtonIcon", time);
+    }
+
+    public void changeButtonIcon()
+    {
+        buttonAudio.SetActive(true);
+        buttonMute.SetActive(false);
+    }
+
+}
--- Visual Anatomy AR/Assets/Script/GameManager.cs
+++ Visual Anatomy AR/Assets/Script/GameManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
 
+
     public void penjelasanTulangManusia()
     {
         AudioManager.singleton.stopSound();
